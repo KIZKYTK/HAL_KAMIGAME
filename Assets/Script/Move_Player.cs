3ddfe7907bc9dc4c4e4c5553160808d20075fc1c
KIZKYTK <kaiduka0127@gmail.com>
2025-04-09 07:01:43 +0900

    Update_Script
    none

--- Assets/Script/Move_Player.cs
+++ Assets/Script/Move_Player.cs
@@ -6,16 +6,24 @@ public class Move_Player : MonoBehaviour
 {
     [SerializeField, Header("ˆÚ“®‘¬“x")]
     private float speedFloat;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // rigidbody‚Ìî•ñ‚ğæ“¾
+        rb=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // A/D or ©/¨
+        float mx = Input.GetAxisRaw("Horizontal");
+        // W/S or ª/«
+        float my = Input.GetAxisRaw("Vertical");
+
+        Vector2 m_pos = new Vector2(mx, my).normalized;
+        rb.velocity = m_pos * speedFloat;
     }
 }
