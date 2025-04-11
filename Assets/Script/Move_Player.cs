91d77d5cf59965ae633d5eacd5d9d00cb67ed608
KIZKYTK <kaiduka0127@gmail.com>
2025-04-11 20:27:07 +0900

    update
    none

--- Assets/Script/Move_Player.cs
+++ Assets/Script/Move_Player.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 public class Move_Player : MonoBehaviour
 {
     [SerializeField, Header("移動速度")]
-    private float speedFloat;
+    public float speedFloat;
+    [SerializeField, Header("ジャンプ力")]
+    public float jumpPower;
+    [SerializeField, Header("Transform")]
+    public new Transform transform;
+    [SerializeField, Header("LayerMask")]
+    public LayerMask layer;
+    [SerializeField, Header("Raduis")]
+    private float radius = 0.3f;
+
+    // リジットボディ情報取得用変数
     private Rigidbody2D rb;
+    // 地面との接触フラグ
+    private bool touchFg;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +30,28 @@ public class Move_Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // A/D or ←/→
-        float mx = Input.GetAxisRaw("Horizontal");
-        // W/S or ↑/↓
-        float my = Input.GetAxisRaw("Vertical");
+        touchFg = Physics2D.OverlapCircle(transform.position, radius, layer);
+        Debug.Log("touchFg: " + touchFg);
 
-        Vector2 m_pos = new Vector2(mx, my).normalized;
-        rb.velocity = m_pos * speedFloat;
+        // A/D or ←/→ で横移動
+        float mx = Input.GetAxisRaw("Horizontal");
+        // 横移動のみ更新
+        rb.velocity = new Vector2(mx * speedFloat, rb.velocity.y);
+
+        // ジャンプ処理
+        if(Input.GetButtonDown("Jump") && touchFg)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+    }
+
+    // 判定範囲を可視化
+    private void OnDrawGizmosSelected()
+    {
+        if (transform != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, radius);
+        }
     }
 }
